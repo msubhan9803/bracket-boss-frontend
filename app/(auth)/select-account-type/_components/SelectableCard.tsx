5051f6bf0d27249d3aec5f37a9fde3e286792f2e
msubhan9803 <mohammadsubhan9803@gmail.com>
2024-09-17 15:59:24 +0500

    feat: Select Account Type page added;

---
+++ app/(auth)/select-account-type/_components/SelectableCard.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { RadioGroupItem } from "@/components/ui/radio-group";
+import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
+
+interface SelectableCardProps {
+  id: string;
+  value: string;
+  label: string;
+  icon: React.ReactNode;
+}
+
+const SelectableCard: React.FC<SelectableCardProps> = ({
+  id,
+  value,
+  label,
+  icon,
+}) => {
+  return (
+    <div>
+      <RadioGroupItem value={value} id={id} className="peer sr-only" />
+      <Label
+        htmlFor={id}
+        className={cn(
+          "flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-8 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary",
+          "[&:has([data-state=checked])]:border-primary"
+        )}
+      >
+        {icon}
+        <span className="mt-3">{label}</span>
+      </Label>
+    </div>
+  );
+};
+
+export default SelectableCard;
--- app/(auth)/select-account-type/page.tsx
+++ app/(auth)/select-account-type/page.tsx
@@ -1,11 +1,29 @@
 import React from "react";
+import { RadioGroup } from "@/components/ui/radio-group";
+import { FaRegBuilding, FaUser } from "react-icons/fa";
+import SelectableCard from "./_components/SelectableCard";
 
-export default async function SelectAccountType() {
+export default function SelectAccountType() {
   return (
     <div className="mx-auto grid w-[350px] gap-6">
       <div className="grid gap-2 text-center">
         <h1 className="text-3xl font-bold">Select your account type</h1>
       </div>
+
+      <RadioGroup defaultValue="club" className="grid grid-cols-2 gap-4">
+        <SelectableCard
+          id="club"
+          value="club"
+          label="Club"
+          icon={<FaRegBuilding className="h-6 w-6" />}
+        />
+        <SelectableCard
+          id="player"
+          value="player"
+          label="Player"
+          icon={<FaUser className="h-6 w-6" />}
+        />
+      </RadioGroup>
     </div>
   );
 }
