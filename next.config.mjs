f363f22ec0b18ef0e9a5f33ef419cc57a57e141f
msubhan9803 <mohammadsubhan9803@gmail.com>
2024-09-19 12:57:10 +0500

    chore: Next Js Bundler Optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
+import bundleAnalyzer from '@next/bundle-analyzer';
+
 const nextConfig = {
-    images: {
-      domains: ['i.pravatar.cc'],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: ['i.pravatar.cc'],
+  },
+};
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const config = withBundleAnalyzer(nextConfig);
+
+export default config;
