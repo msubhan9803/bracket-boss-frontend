c9df4d00c85fde33eee6332f067293bcc00da298
msubhan9803 <mohammadsubhan9803@gmail.com>
2025-04-08 18:03:30 +0500

    feat: Patched image not being served by next js config;

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*", // Allow images from all domains
+      },
+    ],
+  },
+};
+
+export default nextConfig;
